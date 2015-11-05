caeeb66cd9862b9674fce8c81cc7cbbeb688a321
Diego Mazala <diegomazala@gmail.com>
2015-11-05 15:43:52 -0200

    Improved command line to choose binary or ascii format when saving pcd files

--- src/App/KinectGrabberSave.cpp
+++ src/App/KinectGrabberSave.cpp
@@ -12,25 +12,22 @@
 #include <pcl/io/pcd_io.h>
 
 
-std::string file_root;
+std::string folder_output;
 uint32_t number_to_grab = 1, count = 0;
+bool binary_format = true;
 
 void savecloud(const pcl::PointCloud<pcl::PointXYZRGB>::ConstPtr& cloud)
 {
 	if (count < number_to_grab)
 	{
-		boost::filesystem::path path = file_root;
-		path.replace_extension("");
-
 		std::stringstream filename;
-		filename << path << count << ".pcd";
-		std::cout << "Saving to " << filename.str();
-		pcl::io::savePCDFile(filename.str(), *cloud, true);
-
+		filename << folder_output << '/' << count << ".pcd";
+		std::cout << "Saving to " << filename.str() << std::endl;
+		pcl::io::savePCDFile(filename.str(), *cloud, binary_format);
 		++count;
 	}
 
-	std::cout << ". [" << count << "] frames saved" << std::endl;
+	std::cout << "[" << count << "] frames saved" << std::endl;
 }
 
 
@@ -42,8 +39,9 @@ int main(int argc, char* argv[])
 
 	desc.add_options()
 		("help", "produce help message")
-		("output,o", boost::program_options::value<std::string>(&file_root)->required(), "output pcd ")
+		("folder_output,o", boost::program_options::value<std::string>(&folder_output)->required(), "output pcd ")
 		("number,n", boost::program_options::value<uint32_t>(&number_to_grab)->default_value(1), "number of pcd to grab")
+		("binary_format,b", boost::program_options::value<bool>(&binary_format)->default_value(true), "binary format")
 		;
 
 	boost::program_options::positional_options_description p;
@@ -65,6 +63,17 @@ int main(int argc, char* argv[])
 	{
 		std::cout << desc << std::endl;
 		return 0;
+	}
+
+
+	if (!boost::filesystem::is_directory(folder_output))		// Check if the directory exists
+	{
+		std::cout << "Creating folder '" << folder_output << "'" << std::endl;
+		if (!boost::filesystem::create_directory(folder_output))
+		{
+			std::cerr << "Could not create output folder '" << folder_output << "'. Abort." << std::endl;
+			return EXIT_FAILURE;
+		}
 	}
 
 
