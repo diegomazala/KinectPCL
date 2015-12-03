24d474157894d0c3886961debd88c3726c42aa0a
Diego Mazala <diegomazala@gmail.com>
2015-12-03 19:36:41 -0200

    Inserted test with mesh rotation

--- src/Test/IcpTest.cpp
+++ src/Test/IcpTest.cpp
@@ -14,6 +14,13 @@ using namespace Microsoft::VisualStudio::CppUnitTestFramework;
 #include <pcl/visualization/pcl_visualizer.h>
 #include <pcl/console/time.h>   // TicToc
 
+// Converts degrees to radians.
+#define DegToRad(angle_degrees) (angle_degrees * M_PI / 180.0)
+
+// Converts radians to degrees.
+#define RadToDeg(angle_radians) (angle_radians * 180.0 / M_PI)
+
+
 namespace pcl
 {		
 	TEST_CLASS(icp)
@@ -52,6 +59,78 @@ namespace pcl
 				pcl::PCDWriter w;
 				Assert::IsTrue(w.writeBinaryCompressed(filename.str(), *cloud_transformed) > -1, L"\n<TestCupCloud could not save output file>\n", LINE_INFO());
 			}
+		}
+
+
+		TEST_METHOD(TestRotateCloud)
+		{
+			const std::string filepath = "../../data/thai_lion/thai_lion.pcd";
+			const std::string basename = "thai_lion";
+
+			pcl::PointCloud<pcl::PointXYZ>::Ptr cloud_input(new pcl::PointCloud<pcl::PointXYZ>);  // point cloud transformed
+			pcl::PointCloud<pcl::PointXYZ>::Ptr cloud_output(new pcl::PointCloud<pcl::PointXYZ>);  // point cloud transformed
+
+			Assert::IsTrue(pcl::io::loadPCDFile(filepath, *cloud_input) > -1, L"\n<TestRotateCloud could not load input file>\n", LINE_INFO());
+
+			pcl::PCDWriter w;
+			Eigen::Matrix4d transformation_matrix;
+			Eigen::Affine3d rotation;
+
+			// save original
+			w.writeBinaryCompressed(basename + "_00.pcd", *cloud_input);
+
+			// 15º
+			rotation = Eigen::Affine3d::Identity();
+			rotation.rotate(Eigen::AngleAxisd(DegToRad(15.0), Eigen::Vector3d::UnitY()));
+			transformation_matrix.block(0, 0, 3, 3) = rotation.matrix();
+			pcl::transformPointCloud(*cloud_input, *cloud_output, transformation_matrix);
+			// save
+			w.writeBinaryCompressed(basename + "_15.pcd", *cloud_output);
+
+
+			// 30º
+			rotation = Eigen::Affine3d::Identity();
+			rotation.rotate(Eigen::AngleAxisd(DegToRad(30.0), Eigen::Vector3d::UnitY()));
+			transformation_matrix.block(0, 0, 3, 3) = rotation.matrix();
+			pcl::transformPointCloud(*cloud_input, *cloud_output, transformation_matrix);
+			//save
+			w.writeBinaryCompressed(basename + "_30.pcd", *cloud_output);
+
+
+			// 45º
+			rotation = Eigen::Affine3d::Identity();
+			rotation.rotate(Eigen::AngleAxisd(DegToRad(45.0), Eigen::Vector3d::UnitY()));
+			transformation_matrix.block(0, 0, 3, 3) = rotation.matrix();
+			pcl::transformPointCloud(*cloud_input, *cloud_output, transformation_matrix);
+			//save
+			w.writeBinaryCompressed(basename + "_45.pcd", *cloud_output);
+
+
+			// 60º
+			rotation = Eigen::Affine3d::Identity();
+			rotation.rotate(Eigen::AngleAxisd(DegToRad(60.0), Eigen::Vector3d::UnitY()));
+			transformation_matrix.block(0, 0, 3, 3) = rotation.matrix();
+			pcl::transformPointCloud(*cloud_input, *cloud_output, transformation_matrix);
+			//save
+			w.writeBinaryCompressed(basename + "_60.pcd", *cloud_output);
+
+
+			// 75º
+			rotation = Eigen::Affine3d::Identity();
+			rotation.rotate(Eigen::AngleAxisd(DegToRad(75.0), Eigen::Vector3d::UnitY()));
+			transformation_matrix.block(0, 0, 3, 3) = rotation.matrix();
+			pcl::transformPointCloud(*cloud_input, *cloud_output, transformation_matrix);
+			//save
+			w.writeBinaryCompressed(basename + "_75.pcd", *cloud_output);
+
+			// 90º
+			rotation = Eigen::Affine3d::Identity();
+			rotation.rotate(Eigen::AngleAxisd(DegToRad(90.0), Eigen::Vector3d::UnitY()));
+			transformation_matrix.block(0, 0, 3, 3) = rotation.matrix();
+			pcl::transformPointCloud(*cloud_input, *cloud_output, transformation_matrix);
+			//save
+			w.writeBinaryCompressed(basename + "_90.pcd", *cloud_output);
+			
 		}
 
 	};
